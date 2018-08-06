70d72aeb9425b8226e9748f3d3b4bef2951cbf0f
Ruen Zapanta <ruenzapanta@SP-RZapanta-MacBookPro2017.local>
2018-08-07 00:23:33 +0800

    redirect home to swagger ui

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -1,15 +1,17 @@
 package com.example.controllers;
 
-
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
+
     @RequestMapping(method= RequestMethod.GET)
-    String index(){
-        return "index";
+    void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("swagger-ui.html");
     }
 }
